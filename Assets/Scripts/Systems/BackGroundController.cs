acd4ea1b972724c0da111a2b8e5aa94722d4fd79
Megu-ming <dudwogkrrmq@gmail.com>
2025-08-14 11:18:40 +0900

    배경 무한하게 끊이지 않도록 수정/ CineCam 추가

---
+++ Assets/Scripts/Systems/BackGroundController.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class BackGroundController : MonoBehaviour
+{
+    private float startPos, length;
+    public GameObject cam;
+    public float parallaxEffect;
+
+    void Start()
+    {
+        startPos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float distance = (cam.transform.position.x * parallaxEffect); // 0 = move with cam , 1 = won't move
+        float movement = cam.transform.position.x * (1 - parallaxEffect);
+
+        transform.position = new Vector3(startPos + distance, transform.position.y, transform.position.z);
+
+        if(movement>startPos + length)
+        {
+            startPos += length;
+        }
+        else if(movement < startPos - length)
+        {
+            startPos -= length;
+        }
+    }
+}
