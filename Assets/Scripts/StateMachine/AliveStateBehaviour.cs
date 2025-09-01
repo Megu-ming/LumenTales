39b0858805d7e4a31a9c9442448d25166357ac7f
ryungee <ryungee@gmail.com>
2025-09-01 18:37:09 +0900

    몬스터 리스폰 시 fadein 이펙트 적용

--- Assets/Scripts/StateMachine/AliveStateBehaviour.cs
+++ Assets/Scripts/StateMachine/AliveStateBehaviour.cs
@@ -1,32 +1,42 @@
+using System;
 using UnityEngine;
 
 public class AliveStateBehaviour : StateMachineBehaviour
 {
     public bool isStateEnter;
     public bool isStateMachineEnter;
+    public float fadeinDuration = 0.5f;
+    private float elapsedTime = 0f;
+    Color startColor;
     SpriteRenderer sprite;
+
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        if(isStateEnter)
+        if (isStateEnter)
         {
             sprite = animator.GetComponent<SpriteRenderer>();
-            sprite.color = Color.white;
+            startColor = sprite.color;
+            elapsedTime = 0f;
         }
     }
     public override void OnStateMachineEnter(Animator animator, int stateMachinePathHash)
     {
-        if (isStateMachineEnter)
-        {
-            sprite = animator.GetComponent<SpriteRenderer>();
-            sprite.color = Color.white;
-        }
+        //if (isStateMachineEnter)
+        //{
+        //    sprite = animator.GetComponent<SpriteRenderer>();
+        //    sprite.color = Color.white;
+        //}
     }
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        elapsedTime += Time.deltaTime;
+
+        float a = Mathf.Clamp01(elapsedTime / fadeinDuration);
+        sprite.color = new Color(startColor.r, startColor.g, startColor.b, a);
+
+    }
 
     //OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
