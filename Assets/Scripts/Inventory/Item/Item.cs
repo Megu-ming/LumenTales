1878da03d795a7e4a021bfba9e02883d42ce8646
ryungee <ryungee@gmail.com>
2025-09-01 19:23:08 +0900

    땅에 떨어진 아이템 플레이어에게 빨려가는 기능 구현

--- Assets/Scripts/Inventory/Item/Item.cs
+++ Assets/Scripts/Inventory/Item/Item.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class Item : MonoBehaviour
@@ -9,20 +10,18 @@ public class Item : MonoBehaviour
     [SerializeField] private int price;
     [SerializeField] private string description;
 
-    [SerializeField] float popForce = 2f;
     Rigidbody2D rb;
     SpriteRenderer sprite;
+
+    [SerializeField] private float suckSpeed = 12f;     // 플레이어로 끌려가는 속도
+    [SerializeField] private float arriveDistance = 0.25f; // 이 거리 이하로 오면 수집 완료
+    [SerializeField] private float fadeTime = 0.15f;    // 수집 순간 페이드아웃 시간
+    [SerializeField] private float absortableTime = 0.5f;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         sprite = GetComponentInChildren<SpriteRenderer>();
-    }
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        //rb.linearVelocity = new Vector2(rb.linearVelocityX, popForce);
     }
 
     public void Init(ItemData inData)
@@ -50,5 +49,56 @@ public class Item : MonoBehaviour
                     break;
             }       
         }
+    }
+
+    public void CollectItem(Transform player)
+    {
+        StartCoroutine(CollectDelayRoutine(player, absortableTime));
+    }
+    private IEnumerator CollectDelayRoutine(Transform player, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        yield return StartCoroutine(SuckToPlayer(player));
+    }
+
+    private IEnumerator SuckToPlayer(Transform player)
+    {
+        if (rb != null)
+        {
+            rb.simulated = false;
+            rb.linearVelocity = Vector2.zero;
+        }
+
+        while (player != null)
+        {
+            // 플레이어 방향으로 이동
+            Vector3 dir = (player.position - transform.position).normalized;
+            transform.position += dir * suckSpeed * Time.deltaTime;
+
+            // 가까워지면 수집 처리
+            if (Vector3.Distance(transform.position, player.position) <= arriveDistance)
+                break;
+
+            yield return null;
+        }
+
+        // TODO : Add to Inventory
+
+        // fadeout
+        if (sprite != null)
+        {
+            Color c = sprite.color;
+            float t = 0;
+            while (t < fadeTime)
+            {
+                t += Time.deltaTime;
+                float k = 1f - (t / fadeTime);
+                sprite.color = new Color(c.r, c.g, c.b, k);
+                yield return null;
+            }
+        }
+
+        Destroy(gameObject);
     }
 }
