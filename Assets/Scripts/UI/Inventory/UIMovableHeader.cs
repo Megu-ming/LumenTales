782b7a1e3ecb983463181bcef85d09cf5e34ed35
Megu-ming <dudwogkrrmq@gmail.com>
2025-09-05 15:55:22 +0900

    헤더 잡고 인벤토리 드래그로 위치 이동하도록 구현

---
+++ Assets/Scripts/UI/Inventory/UIMovableHeader.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UIMovableHeader : MonoBehaviour, IPointerDownHandler, IDragHandler
+{
+    [SerializeField] RectTransform target;
+
+    private Vector2 beginPos;
+    private Vector2 moveBegin;
+
+    private void Awake()
+    {
+        if (target == null)
+            target = transform.parent.GetComponent<RectTransform>();
+    }
+
+    void IPointerDownHandler.OnPointerDown(PointerEventData eventData)
+    {
+        beginPos = target.position;
+        moveBegin = eventData.position;
+    }
+
+    void IDragHandler.OnDrag(PointerEventData eventData)
+    {
+        Vector2 diff = eventData.position - moveBegin;
+        target.position = beginPos + diff;
+    }
+}
