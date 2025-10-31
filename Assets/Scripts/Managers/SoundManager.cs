241f2209ca6eacb627be87e7f8cbc88c40a54f34
Megu-ming <dudwogkrrmq@gmail.com>
2025-10-31 13:17:19 +0900

    사운드 매니저 클래스 추가

--- Assets/Scripts/Managers/SoundManager.cs
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -1,16 +1,60 @@
+using System;
 using UnityEngine;
 
+public enum SoundType
+{
+    ButtonClick,
+    PlayerAttack,
+    PlayerHurt,
+    EnemyHurt,
+    EnemyDie,
+    ItemPickup,
+    LevelUp,
+    BGM,
+    Footstep,
+}
+
+[RequireComponent(typeof(AudioSource)), ExecuteInEditMode]
 public class SoundManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private SoundList[] soundList;
+    private static SoundManager instance;
+    private AudioSource audioSource;
+
+    private void Awake()
     {
-        
+        instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
     }
+
+    public static void PlaySound(SoundType sound, float volume = 1)
+    {
+        AudioClip[] clips = instance.soundList[(int)sound].Sounds;
+        AudioClip randomClip = clips[UnityEngine.Random.Range(0, clips.Length)];
+        instance.audioSource.PlayOneShot(randomClip, volume);
+    }
+
+#if UNITY_EDITOR
+    private void OnEnable()
+    {
+        string[] names = Enum.GetNames(typeof(SoundType));
+        Array.Resize(ref soundList, names.Length);
+        for(int i=0;i<names.Length; i++)
+        {
+            soundList[i].name = names[i];
+        }
+    }
+#endif
+}
+
+[Serializable]
+struct SoundList
+{
+    public AudioClip[] Sounds { get => sounds; }
+    [HideInInspector] public string name;
+    [SerializeField] AudioClip[] sounds;
 }
